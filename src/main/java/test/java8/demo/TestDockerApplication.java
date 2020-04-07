bc45f1e222dfeb8bef0c3f8e4011d711741f602e
Jhon Rodriguez <jerodriguez1608@gmail.com>
2020-04-07 12:58:55 -0500

    test java

---
+++ src/main/java/test/java8/demo/TestDockerApplication.java
@@ -0,0 +1,13 @@
+package test.java8.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/test/java8/demo/controller/Test.java
@@ -0,0 +1,16 @@
+package test.java8.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+
+    @GetMapping("/test")
+    public String obtenerUrl(){
+
+        return "Este es un test";
+    }
+}
